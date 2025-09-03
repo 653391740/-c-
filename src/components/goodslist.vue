261c63f0fe229ce26c33aca1ba29a8cda4fb2a72
yang0043 <653391740@qq.com>
2025-09-03 15:33:28 +0800

    refactor(商品列表): 重构商品列表组件为通用组件并优化首页结构
    将原商品列表组件从首页视图移至通用组件目录，使其可复用
    为商品列表组件添加props参数支持自定义类型、按钮文本和价格显示
    优化首页结构，将商品列表相关逻辑移至首页组件

---
+++ src/components/goodslist.vue
@@ -0,0 +1,123 @@
+<template>
+<div class="goods_list">
+    <ul>
+        <li v-for="value in goodslist"
+            :key="value.id">
+            <div class="left">
+                <img :src="'http://43.138.15.137:4000' + value.img">
+                <p v-if="type">{{ type }}</p>
+            </div>
+            <div class="right">
+                <p>{{ value.goodsname }}</p>
+                <div>
+                    <div class="price">
+                        <div>¥<span>{{ value.price }}</span></div>
+                        <p v-if="isshow">¥{{ value.market_price }}</p>
+                    </div>
+                    <button v-if="button">{{ button }}</button>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    props: {
+        goodslist: {
+            type: Array,
+            default: () => []
+        },
+        type: {
+            type: String,
+            default: ''
+        },
+        button: {
+            type: String,
+            default: ''
+        },
+        isshow: {
+            type: Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style scoped
+    lang="scss">
+    .goods_list {
+        background-color: #f5f5f5;
+
+        ul {
+            li {
+                padding: 8px 16px;
+                font-size: 12px;
+                background-color: #fff;
+                display: flex;
+                gap: 8px;
+
+                .left {
+                    position: relative;
+                    width: 88px;
+                    height: 88px;
+
+                    img {
+                        width: 100%;
+                        height: 100%;
+                        border-radius: 10px;
+                    }
+
+                    p {
+                        position: absolute;
+                        top: 3px;
+                        left: 0;
+                        border-top-right-radius: 10px;
+                        border-bottom-right-radius: 10px;
+                        background-color: #ff6e50;
+                        color: #fff;
+                        padding: 0 8px 0;
+                    }
+                }
+
+                .right {
+                    flex: 1;
+                    display: flex;
+                    flex-direction: column;
+                    justify-content: space-between;
+
+                    &>div {
+                        display: flex;
+                        justify-content: space-between;
+
+                        .price {
+                            display: flex;
+
+                            span {
+                                font-size: 16px;
+                            }
+
+                            p {
+                                margin-top: 4px;
+                                margin-left: 5px;
+
+                                color: #ccc;
+                                text-decoration: line-through;
+                            }
+                        }
+
+                        button {
+                            background-color: #ff6040;
+                            color: #fff;
+                        }
+                    }
+                }
+            }
+        }
+    }
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,26 +1,46 @@
 <template>
 <div class="home">
     <Title title="首页"></Title>
+
     <Head></Head>
     <Banner></Banner>
     <Nav></Nav>
-    <Tuijian></Tuijian> 
-    <Goodslist></Goodslist>
+    <Tuijian></Tuijian>
+    <div class="goods">
+        <nav>
+            <ul>
+                <li v-for="item, index in tabs"
+                    :key="item"
+                    :class="{ 'active': index == currentIndex }"
+                    @click="changeTab(index)">{{ item }}</li>
+            </ul>
+        </nav>
+        <Goodslist :goodslist="goodslist"
+            :type="tabs[currentIndex]"
+            :isshow="true"
+            button="立即抢购"></Goodslist>
+    </div>
 </div>
 </template>
 
 <script>
+import {
+    gethortgoods
+} from '@/api/home'
 import Title from '@/components/title.vue'
 import Head from '@/views/home/head.vue'
 import Banner from '@/views/home/banner.vue'
 import Nav from '@/views/home/nav.vue'
 import Tuijian from '@/views/home/tuijian.vue'
-import Goodslist from '@/views/home/goodslist.vue'
+import Goodslist from '@/components/goodslist.vue'
 
 export default {
     name: 'Home',
     data() {
         return {
+            currentIndex: 0,
+            tabs: ['热门推荐', '上新推荐', '所有商品'],
+            goods_list: [],
         }
     },
     components: {
@@ -30,11 +50,50 @@ export default {
         Nav,
         Tuijian,
         Goodslist,
+    },
+    mounted() {
+        this.gethortgoods()
+    },
+    computed: {
+        goodslist() {
+            if (this.goods_list.length > 0) return this.goods_list[this.currentIndex].content
+        }
+    },
+    methods: {
+        async gethortgoods() {
+            const { code, list } = await gethortgoods()
+            if (code === 200) {
+                this.goods_list = list
+            }
+        },
+        changeTab(index) {
+            this.currentIndex = index
+        }
     }
 }
 </script>
 
 <style scoped
     lang="scss">
-    .home {}
+    .goods {
+        nav {
+            background-color: #fff;
+
+            ul {
+                display: flex;
+
+                li {
+                    flex: 1;
+                    text-align: center;
+                    line-height: 30px;
+                    color: #ff6e50;
+
+                    &.active {
+                        color: #fff;
+                        background-color: #ffa500;
+                    }
+                }
+            }
+        }
+    }
 </style>
